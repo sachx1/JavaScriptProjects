2aeaaf06524977554e422a9cb1c3768e1d519af6
Sachin Khargie <sachx1281@gmail.com>
2022-11-21 13:59:12 -0500

    setting up css styling

--- gptapp/src/App.js
+++ gptapp/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './app.css'
 
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers'
 import { Brand, CTA, Navbar } from './components';
