ff1dbafbefdd3f5ab362919a09851047010c4fcd
Sachin Khargie <sachx1281@gmail.com>
2022-12-05 17:08:51 -0500

    completed features container for gpt3 demo site

--- gptapp/src/containers/features/Features.jsx
+++ gptapp/src/containers/features/Features.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 import './features.css'
+import { Feature } from '../../components';
+
+const featuresData = [
+  {
+    title:'Improving connections globally',
+    text:'We are improving connections by integrating state of the art server technology with AMD Ryzen'
+  },
+
+  {
+    title:'Becoming an industry standard',
+    text:'Everyday our developers are striving to innovate on AI technologies'
+  }, 
+
+  {
+    title:'Applying AI to everyday life', 
+    text:'Our team is actively implementing our AI technology in everyday scenarios'
+  }
+]
 
 const Features = () => {
   return (
-    <div>Features</div>
+    <div className='gpt3__features section__padding' id="features">
+      <div className='gpt3__features-heading'>
+        <h1 className='gradient__text'> The Future is Now and You Just Need To Realize It. Step into the Future Today & Make it Happen</h1>
+        <p>Request Early Access To Get Started</p>
+      </div>
+      <div className='gpt3__features-container'>
+        {featuresData.map((item, index) => (
+          <Feature title={item.title} text={item.text} key={item.title + index}/>
+        ))}
+      </div>
+    </div>
   )
 }
 
